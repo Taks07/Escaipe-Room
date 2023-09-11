8a2722808a03831ddfde6c2026f5391e026b3038
hayley9080 <hsmi309@aucklanduni.ac.nz>
2023-09-12 10:24:07 +1200

    Created title controller and fxml file for main screen, added images.

---
+++ src/main/java/nz/ac/auckland/se206/controllers/MainScreenController.java
@@ -0,0 +1,11 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class MainScreenController extends TitleController {
+
+  @FXML private ImageView flyingrocket;
+  @FXML private ImageView mainbackground;
+  @FXML private ImageView startImage;
+}
