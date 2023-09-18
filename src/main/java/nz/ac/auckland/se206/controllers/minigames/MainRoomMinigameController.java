9826ae1d8b7952ca1db1d636d0e427eb9be4465b
Yutaka <yutakaoishi7@gmail.com>
2023-09-18 23:55:41 +1200

    Created fxml files, and got images for pipes

--- src/main/java/nz/ac/auckland/se206/controllers/minigames/MainRoomMinigameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/minigames/MainRoomMinigameController.java
@@ -1,3 +1,11 @@
 package nz.ac.auckland.se206.controllers.minigames;
 
-public class MainRoomMinigameController extends MinigameController {}
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class MainRoomMinigameController extends MinigameController {
+  @FXML private ImageView image00;
+  @FXML private ImageView image01;
+  @FXML private ImageView image02;
+  @FXML private ImageView image03;
+}
