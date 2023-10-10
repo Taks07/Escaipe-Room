67d20780a91806aeb8cc78bf51a710989abba879
hayley9080 <hsmi309@aucklanduni.ac.nz>
2023-10-10 16:14:18 +1300

    Fixed cloned code issues with room controller and minigame controller. Made minigame controller extend room contoller, and remove fade transition code from minigame controller.

--- src/main/java/nz/ac/auckland/se206/controllers/minigames/MinigameController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/minigames/MinigameController.java
@@ -1,24 +1,24 @@
 package nz.ac.auckland.se206.controllers.minigames;
 
-import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
-import javafx.util.Duration;
 import nz.ac.auckland.se206.GameState;
+import nz.ac.auckland.se206.controllers.rooms.RoomController;
 
 /**
  * Abstract class for minigame controllers. Contains methods for handling the back arrow click and
  * hovering over objects.
  */
-public abstract class MinigameController {
+public abstract class MinigameController extends RoomController {
   @FXML private Rectangle fadeRectangle;
+  @FXML private ImageView arrow1;
 
   protected boolean isSolved;
-  protected FadeTransition ft;
+
+  // protected FadeTransition ft;
 
   /**
    * Returns user back to room.
@@ -48,13 +48,8 @@ public abstract class MinigameController {
    * @param event the mouse event
    */
   @FXML
-  private void hoverObject(MouseEvent event) {
-    Rectangle object = (Rectangle) event.getSource();
-    String objectId = object.getId();
-
-    Scene scene = object.getScene();
-    ImageView image = (ImageView) scene.lookup("#" + objectId);
-    image.setImage(new Image("/images/objects/" + objectId + "_selected.png"));
+  private void hoverArrow(MouseEvent event) {
+    arrow1.setImage(new Image("/images/objects/arrow1_selected.png"));
   }
 
   /**
@@ -63,13 +58,8 @@ public abstract class MinigameController {
    * @param event the mouse event
    */
   @FXML
-  private void unhoverObject(MouseEvent event) {
-    Rectangle object = (Rectangle) event.getSource();
-    String objectId = object.getId();
-
-    Scene scene = object.getScene();
-    ImageView image = (ImageView) scene.lookup("#" + objectId);
-    image.setImage(new Image("/images/objects/" + objectId + ".png"));
+  private void unhoverArrow(MouseEvent event) {
+    arrow1.setImage(new Image("/images/objects/arrow1.png"));
   }
 
   /** Ends minigame, incrementing part counter and setting the game to solved. */
@@ -87,33 +77,5 @@ public abstract class MinigameController {
           GameState.switchRoom(GameState.currRooms.get(GameState.getCurrRoom()));
         });
     ft.play();
-  }
-
-  /** Fades in the room when the player enters. */
-  protected void fadeIn() {
-    fadeRectangle.setOpacity(1.0);
-    fadeRectangle.setVisible(true);
-    ft = new FadeTransition(Duration.millis(300), fadeRectangle);
-    ft.setFromValue(1.0);
-    ft.setToValue(0.0);
-
-    // Makes sure that the fade rectangle is not visible after the fade in
-    ft.setOnFinished(
-        event -> {
-          fadeRectangle.setVisible(false);
-        });
-
-    ft.play();
-  }
-
-  /**
-   * Sets up the fade out for the room. Need to manually play ft after setting destination room in
-   * setOnFinished.
-   */
-  protected void fadeOut() {
-    ft = new FadeTransition(Duration.millis(300), fadeRectangle);
-    ft.setFromValue(0.0);
-    ft.setToValue(1.0);
-    fadeRectangle.setVisible(true);
   }
 }
